1caac5002fb4aa4c1afbe251a48e29efb023f66b
Udith Wijegunavardhana <udith.irng97@gmail.com>
2025-07-19 09:38:26 +0530

    Core: bottom tab configs

---
+++ app/features/home/screens/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => (
+  <View style={styles.container}>
+    <Text style={styles.text}>Home Screen</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  text: { fontSize: 20 },
+});
+
+export default HomeScreen;
---
+++ app/navigation/BottomTabNavigator.tsx
@@ -0,0 +1,46 @@
+// app/navigation/BottomTabNavigator.tsx
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Feather from 'react-native-vector-icons/Feather';
+import HomeScreen from '../features/home/screens/Home';
+import SearchScreen from '../features/search/screens/Search';
+import ProfileScreen from '../features/profile/screens/Profile';
+
+const Tab = createBottomTabNavigator();
+
+const getTabBarIcon = (routeName: string) => {
+  let iconName: string = 'home';
+  switch (routeName) {
+    case 'Home':
+      iconName = 'home';
+      break;
+    case 'Search':
+      iconName = 'search';
+      break;
+    case 'Profile':
+      iconName = 'user';
+      break;
+  }
+
+  return ({color, size}: {color: string; size: number}) => (
+    <Feather name={iconName} color={color} size={size} />
+  );
+};
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarIcon: getTabBarIcon(route.name),
+        tabBarActiveTintColor: '#000',
+        tabBarInactiveTintColor: '#888',
+      })}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabNavigator;
